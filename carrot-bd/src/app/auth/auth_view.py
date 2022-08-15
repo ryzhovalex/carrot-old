928d4a122b87a735a020bab469533852c28089be
ryzhovalex <thed4rkof@gmail.com>
2022-08-16 00:40:36 +0400

    Add views to auth

--- carrot-bd/src/app/auth/auth_view.py
+++ carrot-bd/src/app/auth/auth_view.py
@@ -1,4 +1,5 @@
 from staze import View
+from flask import request
 from app.auth.auth_service import AuthService
 
 
@@ -13,6 +14,17 @@ class RegisterView(View):
 class LoginView(View):
     ROUTE: str = '/login'
 
+    def post(self):
+        token: str = AuthService.instance().login(request.json)
+        return {
+            'user_token': token
+        }
+
 
 class LogoutView(View):
     ROUTE: str = '/logout'
+
+    def post(self):
+        AuthService.instance().logout(request.headers)
+        return {}
+        
